fadce6e9afaa7bd69e171a3399330bc310bcecf6
vt-d <poonadiscord@gmail.com>
2024-05-28 15:01:53 +0000

    yay

--- modules/nixos/kde/default.nix
+++ modules/nixos/kde/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
-  services.xserver.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
   services.xserver.displayManager.sddm.wayland.enable = true;
+  services.xserver.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
 }
